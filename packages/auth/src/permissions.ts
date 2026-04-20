c0513ca29439c2e9509c724010f03cd670864ec0
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-04-20 17:04:31 -0300

    feat(auth): improve permissions protocols with zod infer

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,20 +2,20 @@ import { type AbilityBuilder } from '@casl/ability'
 
 import { AppAbility } from '.'
 import { User } from './models/user'
-
-type Roles = 'ADMIN' | 'MEMBER'
+import { Role } from './roles'
 
 type PermissionsByRole = (
   user: User,
   builder: AbilityBuilder<AppAbility>
 ) => void
 
-export const permissions: Record<Roles, PermissionsByRole> = {
+export const permissions: Record<Role, PermissionsByRole> = {
   ADMIN(_, { can }) {
     can('manage', 'all')
   },
   MEMBER(_, { can }) {
-    can('invite', 'User')
-    can('create', 'Project')
+    // can('invite', 'User')
+    can('manage', 'Project')
   },
+  BILLING() {},
 }
