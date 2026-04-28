e62f292052b3920c0203444fd8086f44e843ff89
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-04-28 09:52:49 -0300

    feat(api): swagger setup

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,8 +1,11 @@
-import fastifyCors from '@fastify/cors'
 import 'dotenv/config'
+
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
-  // jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
@@ -14,6 +17,22 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS app with multi-tenant & RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyCors)
 
