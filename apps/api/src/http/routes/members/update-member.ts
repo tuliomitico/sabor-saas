b89dbd9fb69c44cd3ccc31934d2cc02b3adf3a29
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-05-14 10:53:05 -0300

    feat(api): update member from organization

---
+++ apps/api/src/http/routes/members/update-member.ts
@@ -0,0 +1,76 @@
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function updateMember(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .put(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          tags: ['Members'],
+          summary: 'Update a member',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          body: z.object({
+            role: roleSchema,
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, memberId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const member = await prisma.member.findUnique({
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!member) {
+          throw new BadRequestError('Member not found')
+        }
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('update', 'User')) {
+          throw new UnauthorizedError(
+            `You're not allowed to update this member.`
+          )
+        }
+
+        const { role } = request.body
+
+        await prisma.member.update({
+          data: {
+            role,
+          },
+          where: {
+            id: memberId,
+            organizationId: organization.id,
+          },
+        })
+
+        return reply.status(204).send()
+      }
+    )
+}
