babf8a594768bff54669a3a895512236a687e2b7
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-05-18 16:00:03 -0300

    chore(web): setup shadcn components

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,11 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn('dark font-sans', inter.variable)}>
       <body className={`antialiased`}>{children}</body>
     </html>
   )
