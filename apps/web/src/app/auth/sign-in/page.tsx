705901af0ea08d11a166565ca7c51654f2c8aa3a
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-05-19 15:51:02 -0300

    feat(web): add auth page

---
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+import { GitHub } from '@/assets/github'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+export default function SignInPage() {
+  return (
+    <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="email">Password</Label>
+        <Input name="password" type="password" id="password" />
+        <Link
+          href="/auth/forgot-password"
+          className="text-foreground text-xs font-medium hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+      <Button type="submit" className="w-full">
+        Sign in with e-mail
+      </Button>
+      <Separator />
+      <Button type="submit" variant="outline" className="w-full">
+        <GitHub className="mr-2 size-4 dark:invert" />
+        Sign in with GitHub
+      </Button>
+    </form>
+  )
+}
