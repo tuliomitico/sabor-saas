9d2ec00c03bee4625213411051de2fe7124f3254
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-05-20 18:28:08 -0300

    feat(web): add forgot password and sign up pages

--- apps/web/src/app/auth/sign-up/page.tsx
+++ apps/web/src/app/auth/sign-up/page.tsx
@@ -6,9 +6,13 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 
-export default function SignInPage() {
+export default function SignUpPage() {
   return (
     <form action="" className="space-y-4">
+      <div className="space-y-1">
+        <Label htmlFor="name">Name</Label>
+        <Input name="name" id="name" />
+      </div>
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
@@ -16,23 +20,25 @@ export default function SignInPage() {
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
-        <Link
-          href="/auth/forgot-password"
-          className="text-foreground text-xs font-medium hover:underline"
-        >
-          Forgot your password?
-        </Link>
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password_confirmation">Confirm your password</Label>
+        <Input
+          name="password_confirmation"
+          type="password"
+          id="password_confirmation"
+        />
       </div>
       <Button type="submit" className="w-full">
-        Sign in with e-mail
+        Create account
       </Button>
       <Button variant="link" className="w-full" size="sm">
-        <Link href="/auth/sign-up">Create new account</Link>
+        <Link href="/auth/sign-in">Already registered? Sign in</Link>
       </Button>
       <Separator />
       <Button type="submit" variant="outline" className="w-full">
         <GitHub className="mr-2 size-4 dark:invert" />
-        Sign in with GitHub
+        Sign up with GitHub
       </Button>
     </form>
   )
