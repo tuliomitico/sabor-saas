a37236ac26e50034bc84e2ffa23f0d3c6d4eafea
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-05-25 09:59:02 -0300

    fix(web): avoid reset form

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -2,7 +2,7 @@
 
 import { AlertTriangle, Loader2 } from 'lucide-react'
 import Link from 'next/link'
-import { useActionState } from 'react'
+import { type SubmitEvent, useState, useTransition } from 'react'
 
 import { GitHub } from '@/assets/github'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
@@ -14,13 +14,33 @@ import { Separator } from '@/components/ui/separator'
 import { signInWithEmailAndPassword } from './actions'
 
 export function SignInForm() {
-  const [{ errors, message, success }, formAction, isPending] = useActionState(
-    signInWithEmailAndPassword,
-    { success: false, message: null, errors: null }
-  )
+  const [isPending, startTransition] = useTransition()
+
+  const [{ success, message, errors }, setFormState] = useState<{
+    success: boolean
+    message: string | null
+    errors: Record<string, string[]> | null
+  }>({
+    errors: null,
+    message: null,
+    success: false,
+  })
+
+  async function handleSignIn(event: SubmitEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const form = event.currentTarget
+
+    const data = new FormData(form)
+
+    startTransition(async () => {
+      const state = await signInWithEmailAndPassword(data)
+      setFormState(state)
+    })
+  }
 
   return (
-    <form action={formAction} className="space-y-4">
+    <form onSubmit={handleSignIn} className="space-y-4">
       {success === false && message && (
         <Alert variant="destructive">
           <AlertTriangle className="size-4" />
