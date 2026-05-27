319b5a06a0cccd1b8ba32c60470b615530a5c732
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-05-27 16:40:19 -0300

    feat(web): get data from the authorized user

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,8 @@
+import { auth } from '@/auth/auth'
 import { Button } from '@/components/ui/button'
 
-export default function Home() {
-  return <Button>Sign In</Button>
+export default async function Home() {
+  const { user } = await auth()
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,6 +1,24 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   const cookieStore = await cookies()
   return !!cookieStore.get('token')?.value
+}
+
+export async function auth() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('token')?.value
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  const profile = await getProfile()
+
+  if (!profile.user) redirect('/auth/sign-out')
+
+  return { user: profile.user }
 }
