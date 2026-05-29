7c2f3b3d01d3c56e323deefbfade5a3045114876
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-05-29 10:24:03 -0300

    feat(web): create organization switcher component

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,9 +1,6 @@
-// import { auth } from '@/auth/auth'
 import { Header } from '@/components/header'
 
 export default async function Home() {
-  // const { user } = await auth()
-
   return (
     <div className="py-4">
       <Header />
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,17 +1,17 @@
-import Image from 'next/image'
 import { Slash } from 'lucide-react'
+import Image from 'next/image'
 
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
 
-import { ProfileButton } from './profile-button'
 import { OrganizationSwitcher } from './organization-switcher'
+import { ProfileButton } from './profile-button'
 
 export function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={rocketseatIcon} alt="Logo do site" className="size-6" />
-        <Slash />
+        <Slash className="text-border size-3 -rotate-[24deg]" />
         <OrganizationSwitcher />
       </div>
       <div className="flex items-center gap-4">
