fc9701e226dedb0d9fe9401d381bee7cfdac2c99
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-06-03 10:32:13 -0300

    feat(web): using env variables

--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,8 +1,9 @@
+import { env } from '@saas/env'
 import { CookiesFn, getCookie } from 'cookies-next'
 import ky from 'ky'
 
 export const api = ky.create({
-  baseUrl: 'http://localhost:3333',
+  baseUrl: env.NEXT_PUBLIC_API_URL,
   hooks: {
     beforeRequest: [
       async ({ request }) => {
