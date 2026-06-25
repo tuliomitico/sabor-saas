511f7fb8f2e2681662f7109c16cd4d1eef1a5ad4
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-06-25 16:48:08 -0300

    feat(web): list invites

---
+++ apps/web/src/http/get-invites.ts
@@ -0,0 +1,24 @@
+import { Role } from '@saas/auth'
+
+import { api } from './api-client'
+
+interface GetInvitesResponse {
+  invites: {
+    id: string
+    createdAt: string
+    role: Role
+    email: string
+    author: {
+      name: string | null
+      id: string
+    } | null
+  }[]
+}
+
+export async function getInvites(org: string): Promise<GetInvitesResponse> {
+  const result = await api
+    .get(`organizations/${org}/invites`, { next: { tags: [`${org}/invites`] } })
+    .json<GetInvitesResponse>()
+
+  return result
+}
