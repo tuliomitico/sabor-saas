16a143a13cad30d0d6ae00653eff53beea019224
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2026-07-02 11:20:29 -0300

    chore: add build and start script to api

---
+++ apps/api/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src'],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  noExternal: [
+    '@saas/auth',
+    '@saas/env',
+  ]
+})
